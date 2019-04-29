5299b4cbe4c640e46ec6d92b9ead645ae77aadfe
SM9 <sm9fragdeluxe@gmail.com>
2019-04-29 05:13:10 +0100

    Fix Config.

--- src/main/java/com/sm9/notenoughessence/common/Config/MainConfig.java
+++ src/main/java/com/sm9/notenoughessence/common/Config/MainConfig.java
@@ -23,8 +23,8 @@ public class MainConfig {
         blacklistedMobs.clear();
         mainConfig.load();
         mainConfig.setCategoryPropertyOrder("General", configOrder);
-        debugMode = mainConfig.getBoolean("DebugMode", Configuration.CATEGORY_GENERAL, false, "Prints debug information on hostile entity death");
-        dropChance = mainConfig.getInt("DropChance", Configuration.CATEGORY_GENERAL, 20, 0, 100, "Drop chance for hostiles");
+        debugMode = mainConfig.getBoolean("DebugMode", Configuration.CATEGORY_GENERAL, debugMode, "Prints debug information on hostile entity death");
+        dropChance = mainConfig.getInt("DropChance", Configuration.CATEGORY_GENERAL, dropChance, 0, 100, "Drop chance for hostiles");
         noZombies = mainConfig.getBoolean("NoZombies", Configuration.CATEGORY_GENERAL, noZombies, "Exclude zombies when dropping essence");
         nightOnly = mainConfig.getBoolean("NightOnly", Configuration.CATEGORY_GENERAL, nightOnly, "Only allow essence drops at night");
         meleeOnly = mainConfig.getBoolean("MeleeOnly", Configuration.CATEGORY_GENERAL, meleeOnly, "Only allow essence drops when killed with a melee weapon");
